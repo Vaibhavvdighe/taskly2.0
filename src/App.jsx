57c49eece72ba9d35bbf094a6d8580fcd46ec626
Vaibhav Dighe <90597458+Vaibhavvdighe@users.noreply.github.com>
2025-06-27 12:04:06 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,39 @@
-import React from "react"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Navbar from "./components/Navbar"
-import Container from "./components/Container"
+import Home from "./components/Home"
+import About from "./components/About"
 import Footer from "./components/Footer"
 
 function App() {
 
-  return (
-    <>
-    <div className="bg-zinc-100 flex flex-col justify-between min-h-screen">
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element:<>
       <Navbar/>
-      <main className="flex-grow max-w-screen-lg mx-auto w-full p-4">
-        <Container/>
-      </main>
+      <Home/>
       <Footer/>
+      </>
+    },
+    {
+      path:"/About",
+      element:<>
+      <Navbar/>
+      <About/>
+      <Footer/>
+      </>
+    }
+  ])
+
+  return (
+    <div className="bg-stone-200 flex flex-col justify-between min-h-screen">
+      
+      {/* <main className="flex-grow max-w-screen-lg mx-auto w-full p-4"> */}
+        {/* <Container/> */}
+        <RouterProvider router={router}/>
+      
     </div>
-    </>
+    
   )
 }
 
